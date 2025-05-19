f48855a57ac82543aa13b20a6617ae993d6ff9d2
Jean-2004 <134113752+Jean-2004@users.noreply.github.com>
2025-05-19 19:04:39 -0300

    Project created

---
+++ src/main/java/com/jeanf/javaspringlist/JavaspringlistApplication.java
@@ -0,0 +1,13 @@
+package com.jeanf.javaspringlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaspringlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaspringlistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jeanf/javaspringlist/JavaspringlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeanf.javaspringlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaspringlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
